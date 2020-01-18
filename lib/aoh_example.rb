5b6ea4f0c3f10ea77c85c880e4b31890564aa94b
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-18 15:30:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,8 +7,13 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+  Personality = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUGE_G
+  ]
 end
 
 def literal_aoh
